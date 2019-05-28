ff42746c08d23bc0158870f5cf35df7c2a5e02a8
joewzy <josiahkotey13@gmail.com>
2019-05-28 09:45:24 +0100

    Buzzer & RGB included

--- Espduino_distance_turb.ino
+++ Espduino_distance_turb.ino
@@ -34,6 +34,10 @@ int buf[10],temp;
 int sensorval=0;
 long int avgval;
 
+int buzzer = 19;
+int red = 23;
+int green = 14;
+
 
 void setup(){
   // for the ads1115(analog extender)  
@@ -45,7 +49,18 @@ void setup(){
   Serial.println("  TURBIDITY AND ULTRASONIC SENSORS ");
   Serial.println("Initializing.................................");
   Serial.println("Maxim / Dallas DS18B20 readout program"); 
+
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(buzzer,OUTPUT);
+ 
+  digitalWrite(red,LOW);
+  greenled();
+  
   delay(4000);
+
+
+  
 }
 void loop(){
     myturb();
@@ -69,18 +84,23 @@ void myturb(){
 //senseTurbidity = senseRawValue * (5.0 / 1024.0); //Convert analog data from 0 -1024 to voltage 0 - 5v;
 Serial.print("TURBIDITY VALUE --> "); //Print the output data to the serial
 Serial.println(senseTurbidity);
-delay(500);
+delay(1000);
 
   if (senseTurbidity>3.40 ){
      Serial.println("\t Water is clear \n");
+     greenled();
     }
    
-  if (senseTurbidity<3.40 && senseTurbidity>2.90 ){
+  if (senseTurbidity<3.40 && senseTurbidity>2.61 ){
      Serial.println("\t Water is not clear \n");
+     buzz();
     }
 
-  else if(senseTurbidity<2.90)
+  else if(senseTurbidity<=2.61){
     Serial.println("\t Warning. Water is muddy or very cloudy!!!!!!! \n");
+    redled();
+    buzz();
+  }
   }
 
 
@@ -98,29 +118,34 @@ delay(500);
     digitalWrite(trigPin, LOW);
     // Reads the echoPin, returns the sound wave travel time in microseconds
     duration = pulseIn(echoPin, HIGH);
-    /*
+     //distance=duration / 58;  // Measure Level of unknown distance(height)
     // Calculating the distance for the tank
     mydistance= duration/58;
     distance= tankheight-mydistance;
-    */
-    distance=duration / 58; 
+    
+    //distance=duration / 58; 
     Serial.println(distance);
     // Prints the distance on the Serial Monitor
     Serial.print("Distance: ");
     Serial.println(distance);
-          if (distance<10&& distance>=5){
+          if (distance<=10&& distance>=5){
     Serial.println("The water level: FULL");
+    greenled();
     }
 
-      else if (distance>10 && distance<=16){
+    else if (distance>10 && distance<=16){
     Serial.println("The water level: NORMAL");
+  greenled();
+
     }
 
-      else if (distance>16){
-    Serial.println("The water level: LOW");
+    else if (distance>16){
+      Serial.println("The water level: LOW");
+      buzz();
+      redled();
     }
     
-    delay(500);
+    delay(1000);
 
 
   }
@@ -174,7 +199,7 @@ delay(500);
   temp=DS18B20.getTempCByIndex(0);
   Serial.print("Temperature: ");
   if (temp<=0){
-   float rtemp=25.54;
+   float rtemp=26.25;
    Serial.println(rtemp);
    return rtemp;
    }
@@ -184,3 +209,42 @@ delay(500);
   }
   delay(1000);
   }
+
+
+  void buzz(){
+   int i;
+ for(i=0;i<80;i++)//output a voice of one frequency
+{ 
+    digitalWrite(buzzer,HIGH);//have voice 
+    delay(1);//delay 1ms 
+    digitalWrite(buzzer,LOW);//do not have voice 
+    delay(1);//delay 1ms
+ 
+
+}
+for(i=0;i<100;i++)//output a voice of another frequency
+{ 
+    digitalWrite(buzzer,HIGH);//have voice 
+    delay(2);//delay 2ms 
+    digitalWrite(buzzer,LOW);//without voice 
+    delay(2);//delay 2ms
+}
+    digitalWrite(buzzer,LOW);
+  }
+  
+
+///////////////RGB---Red led ON function//////////////////////
+void redled(){
+
+    digitalWrite(red,HIGH);
+    delay(1000);
+    digitalWrite(red,LOW);
+}
+
+
+////////////////RGB---Green led ON function///////////////////
+void greenled(){
+  digitalWrite(green,HIGH);
+  delay(1000);
+  digitalWrite(green,LOW);
+}
